5002aa93c8aa0c55454b0f7139938a695e826c7b
Jack Pharies <jack.pharies@centre.edu>
2019-09-19 15:27:51 -0400

    A done

--- scrape.js
+++ scrape.js
@@ -3,3 +3,37 @@ var scraper = require('./tools').scraper;
 //scrapes a URL, parses it with parseFcn, and archives the data in a JSON file called filename.
 
 
+/* start of parse fcn code
+
+scraping marvel
+
+*/
+
+
+
+var parse = function($, data)
+{
+    var parent = $(".az-list");
+    var child = parent.find("li");
+    
+    
+   
+    
+    
+    
+    
+    
+    child.toArray().forEach(function(item,index)
+    {
+        data.push($(item).text());
+        
+        
+        
+    })
+   
+ 
+    
+}
+
+
+scraper("https://www.marvel.com/comics/characters", parse, "text.txt");
